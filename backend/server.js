3a80bbea0f455117933514f576ddee507b94f557
Shruti5709 <shrutikumari5709@gmail.com>
2026-05-02 01:18:00 +0530

    fix app initialization error

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-app.use(cors());
+
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
